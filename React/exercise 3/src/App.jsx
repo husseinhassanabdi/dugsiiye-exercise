f6e0575ec89db1b082b3561eef77ad001743df13
Hussein Hassan Abdi <125039384+husseinhassanabdi@users.noreply.github.com>
2026-08-26 07:44:54 +0300

    added exercise 3

---
+++ React/exercise 3/src/App.jsx
@@ -0,0 +1,15 @@
+import UserCard from "./UserCard";
+
+const App = () => {
+    return(
+    <div>
+    <UserCard name = "Xuseen" email="xuseen@gmail.com"/>
+    <UserCard name = "cali" email="cali@gmail.com"/>
+     <UserCard name = "Maxmed" email="maxmed@gmail.com"/>
+    <UserCard name = "yasmiin" email="yasmiin@gmail.com"/>
+
+    </div>
+    )
+}
+
+export default App;
