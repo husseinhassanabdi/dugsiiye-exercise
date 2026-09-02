a30e751bed1f0db234b2afc06997cba50670519e
Hussein Hassan Abdi <125039384+husseinhassanabdi@users.noreply.github.com>
2026-09-02 14:57:14 +0300

    added exercise 8

---
+++ React/exercise 8/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react'
+
+
+  const Countdown = () => {
+   const [initialTime, setInitialTime] = useState(30);
+  const [timeLeft, setTimeLeft] = useState(30);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timerid;
+
+    if (isRunning && timeLeft > 0) {
+      timerid = setInterval(() => {
+        setTimeLeft(prevTime => prevTime - 1)
+      }, 1000)
+    }
+
+     return () => clearInterval(timerid);
+}, [isRunning, timeLeft]);
+
+  const handleStart = () => {
+    if (timeLeft > 0) {
+      setIsRunning(true);
+    }
+  };
+
+
+    const handleStop = () => {
+    setIsRunning(false);
+  };
+
+
+    const handleReset = () => {
+    setIsRunning(false);
+    setTimeLeft(initialTime);
+  };
+
+
+   const handleInputChange = (e) => {
+    const value = Number(e.target.value);
+    setInitialTime(value);
+    setTimeLeft(value);
+    setIsRunning(false);
+  };
+
+
+  return (
+   <div>
+    <h2>Countdown Timer</h2>
+    <label>Set Time (seconds): </label>
+      <input
+        type="number"
+        value={initialTime}
+        onChange={handleInputChange}
+      />
+
+    <p>Time left: {timeLeft} seconds</p>
+
+     <button onClick={handleStart} disabled={isRunning || timeLeft === 0}>
+        Start
+      </button>
+
+
+    <button onClick={handleStop} disabled={!isRunning}>
+        Stop
+      </button>
+    <button onClick={handleReset}>
+        Reset
+      </button>     
+    
+     </div>
+  );
+};
+
+
+export default Countdown;
