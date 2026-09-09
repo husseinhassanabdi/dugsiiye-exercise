d8a6118a4136b63f5b4605f7129de0a051fdc6ff
Hussein Hassan Abdi <125039384+husseinhassanabdi@users.noreply.github.com>
2026-09-09 18:02:46 +0300

    added exercise 18

---
+++ React/exercise 18/src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+  
+// }
+
+// export default App
+
+import ContactApp from './ContactApp'
+
+function App() {
+  return (
+    <div>
+      <ContactApp />
+    </div>
+  )
+}
+
+export default App
